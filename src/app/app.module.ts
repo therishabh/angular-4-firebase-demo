c15c86f60acd61e953fc97fd6dd60a59fec8f1cc
Rishabh Agrawal <therishabhagrawal@gmail.com>
2017-12-09 16:39:45 +0530

    Install Firebase Packages and create connection with angular project with firebase and list our basic data in list format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// enviornments imported.. 
+import { environment } from './../environments/environment';
+
+// firebase module imported..
+import { AngularFireModule } from 'angularfire2'; 
+import { AngularFireDatabaseModule } from 'angularfire2/database'; 
+// import { AngularFireModule } from 'angularfire2'; 
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +17,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
