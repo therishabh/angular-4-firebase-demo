960389b788d3dc8c5340e170d3b0a1b7616c6819
Rishabh Agrawal <therishabhagrawal@gmail.com>
2017-12-09 16:43:47 +0530

    Async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
-	courses : any[];
-	subscribtions : Subscription;
+export class AppComponent {
+	course$;
 
 	constructor(db : AngularFireDatabase) {
-		this.subscribtions = db.list('/courses').subscribe(
-			(response) => {
-				this.courses = response;
-			}
-		);
-	}
-
-	ngOnDestroy() {
-		this.subscribtions.unsubscribe();
+		this.course$ = db.list('/courses');
 	}
 }
